022b7b6a4720733169616e11341b572b19f4d0af
harsh jaiswal <hsjaiswal3110@gmail.com>
2023-03-17 08:50:59 +0530

    backend on process

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const app = express();
+const port = process.env.PORT || 5000;
+const connectDb = require("./config/db");
+app.use(express.json());
+
+dotenv.config();
+connectDb();
+
+const chats = require("./data/data");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+// app.get("/api/chat", (req, res) => {
+//   res.send(chats);
+// });
+
+// app.get("/api/chat/:id", (req, res) => {
+//   const singleChat = chats.find((c) => c._id === req.params.id);
+//   res.send(singleChat);
+// });
+
+app.listen(port, () => {
+  console.log(`Listening on PORT: ${port}`);
+});
